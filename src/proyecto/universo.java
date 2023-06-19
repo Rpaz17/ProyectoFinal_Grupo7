d85106f2078b1186727ffe52fa5766b66206bbb4
pcast <pcast@DESK-E>
2023-06-19 00:27:12 -0600

    lloro

--- src/proyecto/universo.java
+++ src/proyecto/universo.java
@@ -80,6 +80,8 @@ public class universo extends javax.swing.JFrame {
         btn_menu = new javax.swing.JButton();
         historico = new javax.swing.JLabel();
         activos = new javax.swing.JLabel();
+        ganados_heroes = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
         jPanel4 = new javax.swing.JPanel();
         fondo_universo = new javax.swing.JLabel();
 
@@ -113,17 +115,33 @@ public class universo extends javax.swing.JFrame {
                 .addGap(14, 14, 14))
         );
 
+        historico.setBackground(new java.awt.Color(0, 0, 0));
         historico.setFont(new java.awt.Font("Game Of Squids", 0, 48)); // NOI18N
         historico.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         historico.setText("0");
         historico.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         historico.setOpaque(true);
 
+        activos.setBackground(new java.awt.Color(0, 0, 0));
         activos.setFont(new java.awt.Font("Game Of Squids", 0, 48)); // NOI18N
         activos.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         activos.setText("0");
         activos.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         activos.setOpaque(true);
+
+        ganados_heroes.setBackground(new java.awt.Color(0, 0, 0));
+        ganados_heroes.setFont(new java.awt.Font("Game Of Squids", 0, 48)); // NOI18N
+        ganados_heroes.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        ganados_heroes.setText("0");
+        ganados_heroes.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        ganados_heroes.setOpaque(true);
+
+        jLabel1.setBackground(new java.awt.Color(0, 0, 0));
+        jLabel1.setFont(new java.awt.Font("Game Of Squids", 0, 48)); // NOI18N
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("0");
+        jLabel1.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jLabel1.setOpaque(true);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -131,39 +149,35 @@ public class universo extends javax.swing.JFrame {
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 1225, Short.MAX_VALUE))
+                .addGap(0, 1231, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(activos, javax.swing.GroupLayout.PREFERRED_SIZE, 145, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(136, 136, 136)
-                .addComponent(historico, javax.swing.GroupLayout.PREFERRED_SIZE, 143, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(112, 112, 112))
+                .addContainerGap(817, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(ganados_heroes, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(activos, javax.swing.GroupLayout.DEFAULT_SIZE, 145, Short.MAX_VALUE))
+                .addGap(151, 151, 151)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(historico, javax.swing.GroupLayout.DEFAULT_SIZE, 143, Short.MAX_VALUE)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(185, 185, 185))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addGap(279, 279, 279)
+                .addGap(257, 257, 257)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(activos, javax.swing.GroupLayout.DEFAULT_SIZE, 134, Short.MAX_VALUE)
-                    .addComponent(historico, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 420, Short.MAX_VALUE)
+                    .addComponent(historico, javax.swing.GroupLayout.DEFAULT_SIZE, 100, Short.MAX_VALUE)
+                    .addComponent(activos, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(195, 195, 195)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(ganados_heroes, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 171, Short.MAX_VALUE)
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
-        jPanel4.setOpaque(false);
-
-        fondo_universo.setOpaque(true);
-
-        javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
-        jPanel4.setLayout(jPanel4Layout);
-        jPanel4Layout.setHorizontalGroup(
-            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(fondo_universo, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 1435, Short.MAX_VALUE)
-        );
-        jPanel4Layout.setVerticalGroup(
-            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(fondo_universo, javax.swing.GroupLayout.DEFAULT_SIZE, 860, Short.MAX_VALUE)
-        );
+        jPanel4.setBackground(new java.awt.Color(0, 0, 0));
+        jPanel4.add(fondo_universo);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -193,7 +207,9 @@ public class universo extends javax.swing.JFrame {
     private javax.swing.JLabel activos;
     private javax.swing.JButton btn_menu;
     private javax.swing.JLabel fondo_universo;
+    private javax.swing.JLabel ganados_heroes;
     private javax.swing.JLabel historico;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel4;
